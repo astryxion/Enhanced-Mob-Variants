ab6fa699dc02678baaad2568748e9792991a3cd2
Astryxion <s.pergolotti@my.ccsu.edu>
2026-04-18 01:48:41 -0400

    Add files via upload

--- src/main/java/com/astryxion/moremobvariants/EventHandler.java
+++ src/main/java/com/astryxion/moremobvariants/EventHandler.java
@@ -1,17 +1,19 @@
 package com.astryxion.moremobvariants;
 
-import net.minecraft.world.entity.animal.Chicken;
-import net.minecraft.world.entity.animal.Cow;
-import net.minecraft.world.entity.animal.Pig;
-import net.minecraft.world.entity.animal.Sheep;
-import net.minecraft.world.entity.monster.Skeleton;
-import net.minecraft.world.entity.monster.Spider;
-import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.animal.chicken.Chicken;
+import net.minecraft.world.entity.animal.cow.Cow;
+import net.minecraft.world.entity.animal.feline.Cat;
+import net.minecraft.world.entity.animal.pig.Pig;
+import net.minecraft.world.entity.animal.sheep.Sheep;
+import net.minecraft.world.entity.animal.wolf.Wolf;
+import net.minecraft.world.entity.monster.skeleton.Skeleton;
+import net.minecraft.world.entity.monster.spider.CaveSpider;
+import net.minecraft.world.entity.monster.spider.Spider;
+import net.minecraft.world.entity.monster.zombie.Husk;
+import net.minecraft.world.entity.monster.zombie.Zombie;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 
-@EventBusSubscriber(modid = MoreMobVariants.MODID, bus = EventBusSubscriber.Bus.GAME)
 public class EventHandler {
 
     @SubscribeEvent
@@ -22,22 +24,29 @@ public class EventHandler {
         if (event.getEntity() instanceof Cow e && !e.hasData(Registration.COW_VARIANT)) {
             e.setData(Registration.COW_VARIANT, e.getRandom().nextInt(10));
         }
+        if (event.getEntity() instanceof Cat e && !e.hasData(Registration.CAT_VARIANT)) {
+            e.setData(Registration.CAT_VARIANT, e.getRandom().nextInt(6));
+        }
         if (event.getEntity() instanceof Pig e && !e.hasData(Registration.PIG_VARIANT)) {
             e.setData(Registration.PIG_VARIANT, e.getRandom().nextInt(6));
         }
         if (event.getEntity() instanceof Skeleton e && !e.hasData(Registration.SKELETON_VARIANT)) {
             e.setData(Registration.SKELETON_VARIANT, e.getRandom().nextInt(5));
         }
-        if (event.getEntity() instanceof Spider e && !e.hasData(Registration.SPIDER_VARIANT)) {
+        if (event.getEntity() instanceof Spider e && !(e instanceof CaveSpider) && !e.hasData(Registration.SPIDER_VARIANT)) {
             e.setData(Registration.SPIDER_VARIANT, e.getRandom().nextInt(5));
         }
-        if (event.getEntity() instanceof Zombie e && !e.hasData(Registration.ZOMBIE_VARIANT)) {
+        if (event.getEntity() instanceof Zombie e && !(e instanceof Husk) && !e.hasData(Registration.ZOMBIE_VARIANT)) {
             e.setData(Registration.ZOMBIE_VARIANT, e.getRandom().nextInt(9));
         }
 
         // Handle Sheep (7 total: 0 is vanilla, 1-6 are your custom textures)
         if (event.getEntity() instanceof Sheep e && !e.hasData(Registration.SHEEP_VARIANT)) {
             e.setData(Registration.SHEEP_VARIANT, e.getRandom().nextInt(7));
+        }
+        // Handle Wolf (8 outcomes: 0 vanilla, 1-7 custom breeds under textures/entity/wolf/)
+        if (event.getEntity() instanceof Wolf e && !e.hasData(Registration.WOLF_VARIANT)) {
+            e.setData(Registration.WOLF_VARIANT, e.getRandom().nextInt(8));
         }
     }
 }
